992c2c77456dbf39bf6a2109ba9cb99cfd7e7ff7
Jan Niklas Richter <git@janniklasrichter.de>
2021-01-28 19:14:59 +0100

    Increase depth

--- engines/axwchessbot/main.py
+++ engines/axwchessbot/main.py
@@ -5,7 +5,7 @@ from bratko_kopec import test_engine
 import uci
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--depth", default=2, help="provide an integer (default: 2)")
+parser.add_argument("--depth", default=3, help="provide an integer (default: 3)")
 parser.add_argument("--selfplay", default=False, type=bool)
 parser.add_argument("--test", default=False, type=bool)
 args = parser.parse_args()
