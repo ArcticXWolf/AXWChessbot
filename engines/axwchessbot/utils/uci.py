0f8fe631565cb949466828e2f75fc29ec2f3ec9b
Jan Niklas Richter <git@janniklasrichter.de>
2021-03-20 13:31:09 +0100

    Fix score output not in integer

--- engines/axwchessbot/utils/uci.py
+++ engines/axwchessbot/utils/uci.py
@@ -104,7 +104,7 @@ class Uci:
 
             self.debug(f"[{end_search - start_search :.2f}] {str(info)}")
             self.output(
-                f"info score cp {score} depth {info.get('depth_reached', 0)} nodes {info.get('positions_analyzed', 0)}"
+                f"info score cp {int(score)} depth {info.get('depth_reached', 0)} nodes {info.get('positions_analyzed', 0)}"
             )
             self.output(f"bestmove {move.uci()}")
             return
