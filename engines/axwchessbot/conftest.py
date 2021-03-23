88475b57d9869a5cc2313425721181f0993d4e59
Jan Niklas Richter <git@janniklasrichter.de>
2021-03-23 19:57:33 +0100

    Add pytest configuration description

--- engines/axwchessbot/conftest.py
+++ engines/axwchessbot/conftest.py
@@ -8,7 +8,9 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line(
+        "markers", "slow: mark test as slow to run (use --runslow to run)"
+    )
 
 
 def pytest_collection_modifyitems(config, items):
